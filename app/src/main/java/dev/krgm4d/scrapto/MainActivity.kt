a8048716878a54c4370e948f62f7e13ebf7e455f
Kurogoma4D <highdro0479@gmail.com>
2022-06-05 20:18:05 +0900

    add: introduce compose

--- app/src/main/java/dev/krgm4d/scrapto/MainActivity.kt
+++ app/src/main/java/dev/krgm4d/scrapto/MainActivity.kt
@@ -1,11 +1,57 @@
 package dev.krgm4d.scrapto
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            MainView()
+        }
+    }
+}
+
+private fun saveScrapboxName(input: String) {
+    Log.d("MainActivity", input)
+}
+
+@Composable
+private fun MainView() {
+    MaterialTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(title = { Text("Scrap To") })
+            }
+        ) {
+            Column {
+                InputRow()
+            }
+        }
+    }
+}
+
+@Composable
+private fun InputRow() {
+    var text by remember { mutableStateOf("") }
+
+    Row {
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it }
+        )
+        OutlinedButton(
+            onClick = { saveScrapboxName(text) }
+        ) {
+            Text("SAVE")
+        }
     }
 }
